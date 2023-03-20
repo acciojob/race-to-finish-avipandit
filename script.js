96429113fdb2143843221eb1049102dd69d062ab
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-20 12:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+function getRandomTime() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+const promises = Array.from({ length: 5 }, () => {
+  return new Promise((resolve) => {
+    const time = getRandomTime();
+    setTimeout(() => {
+      resolve(time);
+    }, time * 1000);
+  });
+});
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = `The first promise to resolve had a value of ${result}.`;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
